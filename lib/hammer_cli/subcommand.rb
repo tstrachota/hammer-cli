74a68acf0e10f0dae34be9d042838c89d858aa40
Rahul Bajaj <rahulrb0509@gmail.com>
2017-08-30 07:23:46 +0000

    [WIP] Add hidden functionality to subcommands

--- lib/hammer_cli/subcommand.rb
+++ lib/hammer_cli/subcommand.rb
@@ -3,13 +3,35 @@ module HammerCLI
 
   module Subcommand
 
-    class LazyDefinition < Clamp::Subcommand::Definition
+    class DeprecateCommand < Clamp::Subcommand::Definition
 
-      def initialize(names, description, subcommand_class_name, path)
+      def initialize(names, description, subcommand_class_name, hidden = false, deprecated = false)
+        @names = Array(names)
+        @description = description
+        @subcommand_class_name = subcommand_class_name
+        @hidden = hidden
+        @deprecated = deprecated
+      end
+
+      def hidden?
+        @hidden
+      end
+
+      def deprecated?
+        @deprecated
+      end
+
+    end
+
+    class LazyDefinition < DeprecateCommand
+
+      def initialize(names, description, subcommand_class_name, path, hidden = false, deprecated = false)
         @names = Array(names)
         @description = description
         @subcommand_class_name = subcommand_class_name
         @path = path
+        @hidden = hidden
+        @deprecated = deprecated
         @loaded = false
       end
 
@@ -62,17 +84,17 @@ module HammerCLI
         super
       end
 
-      def lazy_subcommand(name, description, subcommand_class, path)
+      def lazy_subcommand(name, description, subcommand_class, path, hidden = false)
         # call original subcommand to ensure command's parameters are set correctly
         # (hammer command SUBCOMMAND [ARGS] ...)
         subcommand(name, description, Class)
         # replace last subcommand definition with correct lazy-loaded one
-        recognised_subcommands[-1] = LazyDefinition.new(name, description, subcommand_class, path)
+        recognised_subcommands[-1] = LazyDefinition.new(name, description, subcommand_class, path, hidden)
       end
 
-      def lazy_subcommand!(name, description, subcommand_class, path)
+      def lazy_subcommand!(name, description, subcommand_class, path, hidden = false)
         remove_subcommand(name)
-        self.lazy_subcommand(name, description, subcommand_class, path)
+        self.lazy_subcommand(name, description, subcommand_class, path, hidden)
         logger.info "subcommand #{name} (#{subcommand_class}) was created."
       end
 
